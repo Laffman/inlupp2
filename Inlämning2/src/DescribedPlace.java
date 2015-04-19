f9908864f771d7ea7abe27e51a3e73ac5e90e28a
Kalle <karl.mortberg@gmail.com>
2015-04-19 11:37:01 +0200

    öhh

--- Inlämning2/src/DescribedPlace.java
+++ Inlämning2/src/DescribedPlace.java
@@ -10,7 +10,7 @@ class DescribedPlace extends Place {
 		this.description = description;
 	}
 	
-	protected void show(Graphics g) {
+	protected void paintComponent(Graphics g) {
 		g.setColor(Color.GREEN);
 		int[] xes = { 0, 25, 50 };
 		int[] yes = { 50, 0, 50 };
